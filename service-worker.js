33cc5d5954ec974f895e63debdc645e66bc22480
riosdavid301-bit <riosdavid301@gmail.com>
2026-04-06 17:16:36 -0400

    Create service-worker.js for PWA offline support

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+'use strict';
+
+// Install a service worker
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('v1').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/styles.css',
+                '/script.js',
+                '/images/logo.png'
+            ]);
+        })
+    );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// Activate a service worker
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = ['v1'];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
